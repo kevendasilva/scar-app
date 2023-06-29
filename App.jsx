6d444a037f5b421783863718d77729789ea7c613
Keven da Silva Gonçalves <dasilvagkeven@gmail.com>
2023-06-29 13:46:56 -0300

    config: setup eslint and prettier

---
+++ App.jsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+export default function App() {
+  const message = "Let's go!";
+
+  return (
+    <View>
+      <Text>{message}</Text>
+    </View>
+  );
+}
