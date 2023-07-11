7a8491d399f3df858ad6918e1eec3d9761b57f07
Keven da Silva Gonçalves <dasilvagkeven@gmail.com>
2023-07-11 04:57:15 -0300

    nav: menu to pages

--- components/Menu.js
+++ components/Menu.js
@@ -4,20 +4,32 @@ import { FontAwesome } from '@expo/vector-icons';
 
 import { styles } from '../styles/Menu';
 
-export function Menu() {
+export function Menu({ navigation }) {
   return (
     <View style={styles.container}>
       <View style={styles.buttons}>
-        <TouchableOpacity style={styles.button}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => navigation.navigate('Index')}
+        >
           <FontAwesome name="home" size={36} color="white" />
         </TouchableOpacity>
-        <TouchableOpacity style={styles.button}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => navigation.navigate('DoorLockForm')}
+        >
           <FontAwesome name="plus" size={36} color="white" />
         </TouchableOpacity>
-        <TouchableOpacity style={styles.button}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => navigation.navigate('EditUser')}
+        >
           <FontAwesome name="gear" size={36} color="white" />
         </TouchableOpacity>
-        <TouchableOpacity style={styles.button}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => navigation.navigate('HomePage')}
+        >
           <FontAwesome name="sign-out" size={36} color="white" />
         </TouchableOpacity>
       </View>
